f091e0bd8f2d3a5c5cefd70810396a6e132609cd
Carl Eriksson <carl.erikssonn@hotmail.com>
2023-06-07 18:56:42 +0200

    Added Trajectory.cs

---
+++ Utility/Trajectory.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+/*
+* curves a raycast, good for grenades, or bulletdrop using raycast
+* Carl Eriksson
+* 2021-04-02
+*/
+
+public class Trajectory : MonoBehaviour {
+    [Range(0.1f, 0.6f)] [SerializeField] private float intervals = 0.1f;
+    [SerializeField] private float incline = 12f;
+    [SerializeField] private float reach = 10f;
+    [Range(0.1f, 0.8f)] [SerializeField] private float rayStopDist = 0.1f;
+
+    private List<Vector3> generatedPoints = new List<Vector3>();
+
+    private void LateUpdate () {
+        CurveSimulation();
+    }
+
+    //Try to calculate a trajectory preditction curve
+    private void CurveSimulation() {
+        generatedPoints = PointGeneration();
+
+        for (int i = 0; i < generatedPoints.Count; i++) {
+            if (i + 1 < generatedPoints.Count) {
+                Debug.DrawLine(generatedPoints[i], generatedPoints[i + 1], Color.blue);
+            }
+        }
+    }
+
+    //collision check function
+    private bool CollisionCheck(Vector3 pos) {
+        Collider[] col = Physics.OverlapSphere(pos, rayStopDist);
+
+        if (col.Length > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    //Generate the points in 3d-space
+    private List<Vector3> PointGeneration() {
+        List<Vector3> points = new List<Vector3>();
+
+        float maxDuration = 5f;
+        float timeStepInterval = intervals;
+        int maxSteps = (int)(maxDuration / timeStepInterval);
+        Vector3 directionVector = Camera.main.transform.forward;
+        Vector3 launchPosition = Camera.main.transform.position + Camera.main.transform.forward;
+
+        float vel = incline * reach * Time.fixedDeltaTime;
+
+        for (int i = 0; i < maxSteps; i++) {
+            //f(y) = (x0 + x*t, y0 + y*t - 9.81t^2/2)
+            Vector3 calculatedPosition = launchPosition + directionVector * vel * i * timeStepInterval;
+            calculatedPosition.y += Physics.gravity.y / 2 * Mathf.Pow(i * timeStepInterval, 2);
+
+            points.Add(calculatedPosition);
+
+            //check if the newly added position collides with something, then stop adding additional positions
+            if (CollisionCheck(calculatedPosition)) {
+                break;
+            }
+        }
+
+        return points;
+    }
+}
